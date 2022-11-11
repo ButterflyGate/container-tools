82454b44141c0935201f644a953b748b992b857b
kyota-tashiro <kyota.HelloWorld@gmail.com>
2022-11-11 11:05:46 +0900

    nextcloud 修正

---
+++ nextcloud/shells/functions.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+this_file=$(basename $0)
+this_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+
+
+function mount() {
+
+
+}
+
+function factoryReset() {
+
+}
+
+function 
--- nextcloud/shells/startup.sh
+++ nextcloud/shells/startup.sh
@@ -1,12 +1,19 @@
-#!/bin/bash
-
-# sudo mount /dev/shinra-bansho/private /mnt/lvm-private
-# sudo mount /dev/shinra-bansho/public /mnt/lvm-public
+#!/bin/zsh
+this_file=$(basename $0)
+this_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
 mountdev="/dev/shinra-bansho/public"
 mountpoint="/mnt/lvm-public"
 mountedpoint=$(findmnt --output=TARGET -n -S /dev/shinra-bansho/public)
+# sudo mount /dev/shinra-bansho/private /mnt/lvm-private
+# sudo mount /dev/shinra-bansho/public /mnt/lvm-public
 
+function startup() {
+
+
+
+    docker-compose up -d
+}
 
 if [[ -n $mountedpoint ]]; then
     if [[ $mountedpoint != $mountpoint ]]; then
@@ -18,4 +25,4 @@ if [[ $mountedpoint != $mountpoint ]]; then
     sudo mount $mountdev $mountpoint
 fi
 
-docker-compose up -d
+startup()
